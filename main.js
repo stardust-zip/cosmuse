2447bd04a237f14a0e84926a87de2c7ea52fccf1
stardust-zip <nguyenhieu0802@proton.me>
2025-12-05 19:59:26 +0700

    feat: done with POST

--- main.js
+++ main.js
@@ -2,35 +2,35 @@ import http from "node:http";
 
 const host = "127.0.0.1";
 const port = "8282";
+const chapters = [
+  {
+    id: 1,
+    title: "Pillar of the World",
+    wordCount: 10231,
+  },
+  {
+    id: 2,
+    title: "The Edge of Sunlight",
+    wordCount: 10231,
+  },
+  {
+    id: 3,
+    title: "Whisper of Snow",
+    wordCount: 10231,
+  },
+  {
+    id: 4,
+    title: "Wind of the West",
+    wordCount: 10231,
+  },
+  {
+    id: 5,
+    title: "The Jewel of Tommorrow",
+    wordCount: 10231,
+  },
+];
 
 const server = http.createServer((req, res) => {
-  const chapters = [
-    {
-      id: 1,
-      title: "Pillar of the World",
-      wordCount: 10231,
-    },
-    {
-      id: 2,
-      title: "The Edge of Sunlight",
-      wordCount: 10231,
-    },
-    {
-      id: 3,
-      title: "Whisper of Snow",
-      wordCount: 10231,
-    },
-    {
-      id: 4,
-      title: "Wind of the West",
-      wordCount: 10231,
-    },
-    {
-      id: 5,
-      title: "The Jewel of Tommorrow",
-      wordCount: 10231,
-    },
-  ];
   switch (req.method) {
     case "GET":
       if (req.url === "/") {
@@ -66,6 +66,38 @@ const server = http.createServer((req, res) => {
       }
       break;
     case "POST":
+      if (req.url === "/chapters") {
+        let body = "";
+
+        req.on("data", (chunk) => {
+          // 'chunk' is a Buffer (binary data).
+          body += chunk.toString();
+          console.log(body);
+        });
+
+        req.on("end", () => {
+          try {
+            const data = JSON.parse(body);
+            const newId =
+              chapters.length > 0 ? chapters[chapters.length - 1].id + 1 : 1;
+            const chapter = {
+              id: newId,
+              title: data.title,
+              wordCount: data.wordCount,
+            };
+            chapters.push(chapter);
+            console.log(chapters);
+            res.statusCode = 201;
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify(chapter));
+          } catch (err) {
+            console.log("Error while parsing JSON: ", err);
+            res.statusCode = 404;
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify({ error: "Failed to parse JSON" }));
+          }
+        });
+      }
       break;
     default:
       console.log("Not implemented yet!");
---
+++ scripts/create_chapter.sh
@@ -0,0 +1,3 @@
+curl -X POST http://127.0.0.1:8282/chapters \
+    -H "Content-Type: application/json" \
+    -d '{"title": "The Stone of Cinderfall", "wordCount": 6231}'
