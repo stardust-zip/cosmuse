a5eb9e59b80f905c66ffce153bebdf6474ceaed3
stardust-zip <nguyenhieu0802@proton.me>
2025-12-06 21:14:45 +0700

    feat: more openapi docs

--- src/docs/openapi.ts
+++ src/docs/openapi.ts
@@ -2,12 +2,57 @@ import {
   OpenApiGeneratorV3,
   OpenAPIRegistry,
 } from "@asteasolutions/zod-to-openapi";
-import { createPostSchema } from "../schemas/post.schema";
-import { OpenAPIGenerator } from "@asteasolutions/zod-to-openapi/dist/openapi-generator";
+import {
+  createPostSchema,
+  PostSchema,
+  PostParamsSchema,
+  updatePostSchema,
+} from "../schemas/post.schema";
+import z from "zod";
 
 export const registry = new OpenAPIRegistry();
-registry.register("Post", createPostSchema);
 
+registry.registerPath({
+  method: "get",
+  path: "/posts",
+  description: "Get all posts.",
+  summary: "get all posts",
+  responses: {
+    200: {
+      description: "Get all post successfully",
+      content: {
+        "application/json": {
+          schema: z.array(PostSchema),
+        },
+      },
+    },
+  },
+});
+
+registry.registerPath({
+  method: "get",
+  path: "/posts/{id}",
+  description: "Get a post by it's id",
+  summary: "Get post by id",
+  request: {
+    params: PostParamsSchema,
+  },
+  responses: {
+    200: {
+      description: "Found the post",
+      content: {
+        "application/json": {
+          schema: PostSchema,
+        },
+      },
+    },
+    404: {
+      description: "Not found.",
+    },
+  },
+});
+
+registry.register("Post", createPostSchema);
 registry.registerPath({
   method: "post",
   path: "/posts",
@@ -27,12 +72,64 @@ registry.registerPath({
       description: "Post created successfully",
       content: {
         "application/json": {
-          schema: createPostSchema, // TODO: make a separete 'ResponseSchema' with ID
+          schema: PostSchema,
         },
       },
     },
     400: {
       description: "Validation Error",
+    },
+  },
+});
+
+registry.register("Put", updatePostSchema);
+registry.registerPath({
+  method: "put",
+  path: "/posts/{id}",
+  description: "update a post based on id",
+  summary: "Update Post",
+  request: {
+    params: PostParamsSchema,
+    body: {
+      content: {
+        "application/json": {
+          schema: updatePostSchema,
+        },
+      },
+    },
+  },
+  responses: {
+    200: {
+      description: "Post updated successfully",
+      content: {
+        "application/json": {
+          schema: PostSchema,
+        },
+      },
+    },
+    400: {
+      description: "Post update failed",
+    },
+  },
+});
+
+registry.registerPath({
+  method: "delete",
+  path: "/posts/{id}",
+  description: "Delete a post based on the id.",
+  summary: "Delete Post",
+  request: {
+    params: PostParamsSchema,
+  },
+  responses: {
+    204: {
+      description: "Post deleted successfully",
+    },
+    400: {
+      description: "Failed to delete post",
+    },
+    404: {
+      description: "Post not found",
     },
   },
 });
--- src/schemas/post.schema.ts
+++ src/schemas/post.schema.ts
@@ -2,21 +2,46 @@ import { z } from "zod";
 import { extendZodWithOpenApi } from "@asteasolutions/zod-to-openapi";
 extendZodWithOpenApi(z);
 
-export const createPostSchema = z.object({
-  title: z
+export const PostSchema = z
+  .object({
+    id: z.number().openapi({ example: 1 }),
+    title: z.string().openapi({ example: "My Title" }),
+    content: z.string().openapi({ example: "My Content" }),
+  })
+  .openapi("Post");
+
+export const PostParamsSchema = z.object({
+  id: z
     .string()
-    .min(1, "Title is required")
-    .openapi({ example: "My First Post" }),
-  content: z
-    .string()
-    .optional()
-    .openapi({ example: "My first post's content." }),
+    .transform((val) => Number(val))
+    .openapi({
+      param: {
+        name: "id",
+        in: "path", // tells openapi this is in the URL, not the body
+      },
+      example: "1",
+    }),
 });
 
-export const updatePostSchema = z.object({
-  title: z
-    .string()
-    .min(1, "Title is required")
-    .openapi({ example: "Updated Title" }),
-  content: z.string().optional().openapi({ example: "Updated Content" }),
-});
+export const createPostSchema = z
+  .object({
+    title: z
+      .string()
+      .min(1, "Title is required")
+      .openapi({ example: "My First Post" }),
+    content: z
+      .string()
+      .optional()
+      .openapi({ example: "My first post's content." }),
+  })
+  .openapi("CreatePost");
+
+export const updatePostSchema = z
+  .object({
+    title: z
+      .string()
+      .min(1, "Title is required")
+      .openapi({ example: "Updated Title" }),
+    content: z.string().optional().openapi({ example: "Updated Content" }),
+  })
+  .openapi("UpdatePost");
